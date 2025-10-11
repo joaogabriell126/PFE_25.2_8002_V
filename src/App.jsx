d46f2a21d40b9b4e0ea72649d35bb9f685f938d9
Gabriel Oliveira <128335898+joaogabriell126@users.noreply.github.com>
2025-10-11 18:28:48 -0300

    feat: Componentização completa dos botões

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import Button from './components/ui/Button/Button.jsx';
+
 function App() {
   return (
-    <h1>Oi, está tudo funcionando</h1>
+    <div>
+      <h1>Exemplos de componentização do botão</h1>
+      <Button variant="primary">Entre Agora</Button>
+      <Button variant="alternative">Entre Agora</Button>
+      <Button variant="danger">Entre Agora</Button>
+
+      <h1>Exemplos de componentização do botão</h1>
+      <Button variant="primary" size='small'>Entre Agora</Button>
+      <Button variant="alternative" size='small'>Entre Agora</Button>
+      <Button variant="danger" size='small'>Entre Agora</Button>
+    </div>
   );
 }
 
--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,9 +1,17 @@
-// Aqui estou importando o aquivo Button.module.scss
-import "./Button.module.scss";
+// 1. A importação agora coloca os estilos no objeto 'styles'
+import styles from "./Button.module.scss";
 
-export default function Button({children, onClick, type = 'button', variant = 'primary'}){
-    return(
-        <button className={`btn btn--${variant}`} type={type} onClick={onClick}>
+export default function Button({ children, onClick, type = 'button', variant = 'primary', size = 'medium' }) {
+
+    const buttonClass = `
+        ${styles.btn} 
+        ${styles[`btn--${variant}`]} 
+        ${size === 'small' ? styles['btn--small'] : ''}
+    `;
+
+    return (
+        // O .trim() no final é opcional, mas remove espaços extras
+        <button className={buttonClass.trim()} type={type} onClick={onClick}>
             {children}
         </button>
     )
