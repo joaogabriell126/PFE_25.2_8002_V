e4f3ad2ead1794c63b785f278b565e498bc73a12
joaogabriell126 <joaogabriell.126@gmail.com>
2025-10-12 16:02:19 -0300

    feat: Componente textarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
 import Button from './components/ui/Button/Button.jsx';
 import Modal from './components/ui/Modal/Modal.jsx';
+import Textarea from './components/ui/Textarea/Textarea.jsx';
 
-function App() {
-  return (
-    <div>
-      <h1>Exemplos de componentização do botão - Grande</h1>
-      <Button variant="primary">Entre Agora</Button>
-      <Button variant="alternative">Entre Agora</Button>
-      <Button variant="danger">Entre Agora</Button>
+  function App() {
+    return (
+      <div>
+        <h1>Exemplos de componentização do botão - Grande</h1>
+        <Button variant="primary">Entre Agora</Button>
+        <Button variant="alternative">Entre Agora</Button>
+        <Button variant="danger">Entre Agora</Button>
 
-      <h1>Exemplos de componentização do botão - médio</h1>
-      <Button variant="primary" size='medium'>Entre Agora</Button>
-      <Button variant="alternative" size='medium'>Entre Agora</Button>
-      <Button variant="danger" size='medium'>Entre Agora</Button>
+        <h1>Exemplos de componentização do botão - médio</h1>
+        <Button variant="primary" size='medium'>Entre Agora</Button>
+        <Button variant="alternative" size='medium'>Entre Agora</Button>
+        <Button variant="danger" size='medium'>Entre Agora</Button>
 
-      <h1>Exemplos de componentização do botão - Pequeno</h1>
-      <Button variant="primary" size='small'>Entre Agora</Button>
-      <Button variant="alternative" size='small'>Entre Agora</Button>
-      <Button variant="danger" size='small'>Entre Agora</Button>
+        <h1>Exemplos de componentização do botão - Pequeno</h1>
+        <Button variant="primary" size='small'>Entre Agora</Button>
+        <Button variant="alternative" size='small'>Entre Agora</Button>
+        <Button variant="danger" size='small'>Entre Agora</Button>
 
-      <Modal>Tem certeza que deseja remover essa vaga permanentemente?</Modal>
-    </div>
-  );
-}
+        {/* <Modal>Tem certeza que deseja remover essa vaga permanentemente?</Modal> */}
+
+        {/* Usando o componente Textarea */}
+        <Textarea
+          label="Sua Mensagem"
+          name="mensagem" // O 'name' deve ser igual à chave no estado 'formData'
+          placeholder="Digite sua mensagem aqui..."
+          rows={6}
+        />
+      </div>
+    );
+  }
 
 export default App;
--- src/components/ui/Textarea/Textarea.jsx
+++ src/components/ui/Textarea/Textarea.jsx
@@ -1,7 +1,11 @@
-export default function Textarea ({label, name, value, onChange, placeholder, rows = 5, error }){
+import styles from './Textarea.module.scss';
+
+export default function Textarea ({label, name, placeholder, rows = 5, error }){
     return(
-        <div>
-            <label htmlFor=""></label>
+        <div className={styles.textarea}>
+            <label htmlFor={name} className={styles.textarea__label}>{label}</label>
+            <textarea label={label} name={name} id={name} className={styles.textarea__input} placeholder={placeholder} rows={rows}></textarea>
+            {error && <span className={styles.textarea__error}>{error}</span>}
         </div>
     )
 }
